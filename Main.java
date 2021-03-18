7b46060b8453e03a453859662b4a77e0d9e6a8d5
kaskinas <kaskinas@yahoo.fr>
2021-03-18 09:16:06 +0000

    second commit

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
   public static void main(String[] args) {
-   System.out.println("Hello Asseke canis");
+   System.out.println("Hello Asseke canis en text de jenkins");
+
 
   }
 
